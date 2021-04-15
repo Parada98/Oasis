e6e1bd573c1d51d6ab0336301e23b7b3e7705bd3
Parada98 <70612835+Parada98@users.noreply.github.com>
2021-04-15 18:05:21 -0500

    Delete public_html directory

--- public_html/views/inicio/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $titulo = 'Oasis';
-    include ("php/cabecera.php"); 
-    include ("php/menu-default.php");
-    include ("php/inicio.php");
-    include ("html/body-aside.html");
-    include ("html/body-final.html");
-?>
